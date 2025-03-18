eaba2112a0e00ae7c9b7c2a7b9695070836294ab
Xiao Zhang <xiaoz@comoco.xyz>
2025-03-18 19:42:32 +0800

    update

--- setup.py
+++ setup.py
@@ -57,8 +57,12 @@ class CMakeBuildExt(build_ext):
         shutil.copytree(source_lib_path, lib_path)
         
         # test
-        source_test_path = os.path.join(PROJECT_DIR, "../requirements/cmeel.prefix/lib/libconsole_bridge.so")
-        shutil.copy(source_test_path, lib_path)
+        source_test_paths = [
+                os.path.join(PROJECT_DIR, "../requirements/cmeel.prefix/lib/libconsole_bridge.so"),
+                os.path.join(PROJECT_DIR, "../requirements/cmeel.prefix/lib/liburdfdom_model.so")
+        ]
+        for lib in source_test_paths:
+            shutil.copy(lib, lib_path)
         
         # bin
         bin_path = os.path.join(self.build_lib, "cmeel.prefix", "bin")
